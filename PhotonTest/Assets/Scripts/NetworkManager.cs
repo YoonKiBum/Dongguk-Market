cc3371d5cedd25172c8caeba52965227eb8d7455
kwonbokeun <royallouis4069jkl@gmail.com>
2022-05-27 10:48:33 +0900

    store

--- PhotonTest/Assets/Scripts/NetworkManager.cs
+++ PhotonTest/Assets/Scripts/NetworkManager.cs
@@ -5,19 +5,39 @@ using PlayFab.ClientModels;
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
+using System;
+using System.Linq;
+
 
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
     public GameObject LoginObject;
     public InputField EmailInput, PasswordInput, UsernameInput;
-    public GameObject register;
-    public GameObject purchase;
-
 
     public Text StatusText;//접속상태
                            // public InputField NickNameInput;//닉네임
     public GameObject Cube;//플레이어 생성 위치
     public string gameVersion = "1.0";//게임 버전
+
+
+    public static int MAX_PLAYER = 10;
+    public static int MAX_Items = 10;
+    GameObject ItemTemplate;
+    [SerializeField] Transform ShopScrollView;
+    public List<GameObject> Shop_Items = new List<GameObject>();
+    public GameObject Sample_Item; // 상점에 인스턴스 생성하기 위해 만들어 놓은 sample
+    public GameObject shop;
+    public GameObject register;
+    public InputField NameInput, PriceInput, InfoInput;
+
+    Hashtable Player_Items = new Hashtable();
+
+    public PlayerLeaderboardEntry MyPlayFabInfo;
+    public List<PlayerLeaderboardEntry> PlayFabUserList = new List<PlayerLeaderboardEntry>();
+
+
 
     bool Flag = false;
 
@@ -31,6 +51,9 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     {
         //포톤 게임 버전을 gameVersion으로 설정한다.
         PhotonNetwork.GameVersion = this.gameVersion;
+        Sample_Item.SetActive(false);
+        shop.SetActive(false);
+        register.SetActive(false);
     }
 
     public void LoginBtn()
@@ -42,29 +65,185 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 
     public void RegisterBtn()
     {
-        var request = new RegisterPlayFabUserRequest { Email = EmailInput.text, Password = PasswordInput.text, Username = UsernameInput.text };
-        PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnRegisterFailure);
+        var request = new RegisterPlayFabUserRequest { Email = EmailInput.text, Password = PasswordInput.text, Username = UsernameInput.text, DisplayName = UsernameInput.text };
+        PlayFabClientAPI.RegisterPlayFabUser(request, (result) => { print("회원가입 성공"); SetStat(); SetData("0"); }, (error) => print("회원가입 실패"));
     }
 
+    void SetStat()
+    {
+        var request = new UpdatePlayerStatisticsRequest { Statistics = new List<StatisticUpdate> { new StatisticUpdate { StatisticName = "IDInfo", Value = 0 } } };
+        PlayFabClientAPI.UpdatePlayerStatistics(request, (result) => { }, (error) => print("값 저장실패"));
+    }
+
+    void SetData(string initNo)
+    {
+        var request = new UpdateUserDataRequest
+        {
+            Data = new Dictionary<string, string>() { { "ItemNo", initNo } },
+            Permission = UserDataPermission.Public
+        };
+        PlayFabClientAPI.UpdateUserData(request, (result) => print("데이터 저장 성공"), (error) => print("데이터 저장 실패"));
+    }
+
+    void GetLeaderboard(string myID)
+    {
+        PlayFabUserList.Clear();
+
+        for (int i = 0; i < MAX_PLAYER; i++)
+        {
+            var request = new GetLeaderboardRequest
+            {
+                StartPosition = i * 100,
+                StatisticName = "IDInfo",
+                MaxResultsCount = 100,
+                ProfileConstraints = new PlayerProfileViewConstraints() { ShowDisplayName = true }
+            };
+            PlayFabClientAPI.GetLeaderboard(request, (result) =>
+            {
+                if (result.Leaderboard.Count == 0) return;
+                for (int j = 0; j < result.Leaderboard.Count; j++)
+                {
+                    PlayFabUserList.Add(result.Leaderboard[j]);
+                    if (result.Leaderboard[j].PlayFabId == myID) MyPlayFabInfo = result.Leaderboard[j];
+                }
+
+                getUserItemNo();
+            },
+            (error) => { });
+        }
+    }
+
+    public void getUserItemNo()
+    {
+        for (int i = 0; i < PlayFabUserList.Count; i++)
+        {
+            var request = new GetUserDataRequest() { PlayFabId = PlayFabUserList[i].PlayFabId };
+            PlayFabClientAPI.GetUserData(request,
+                (result) => {
+                    if (result.Data == null || !result.Data.ContainsKey("ItemNo")) Debug.Log("No Data");
+                    else
+                    {
+                        string t = result.Data["ItemNo"].Value;
+
+                        if (!Player_Items.ContainsKey(PlayFabUserList[i].PlayFabId))
+                        {
+                            Player_Items.Add(PlayFabUserList[i].PlayFabId, t);
+                        }
+                        else
+                            Player_Items[PlayFabUserList[i].PlayFabId] = t;
+                    }
+                }, (error) => {
+                    Debug.Log("Got error retrieving user data:");
+                });
+        }
+    }
+
+
+
+    public void UploadBtn()
+    {
+        string name = NameInput.text;
+        string price = PriceInput.text;
+        string Info = InfoInput.text;
+
+        string data = name + "`" + price + "`" + Info;
+        string No = "0";
+
+        if (Player_Items.ContainsKey(MyPlayFabInfo.PlayFabId))
+        {
+            No = Player_Items[MyPlayFabInfo.PlayFabId].ToString();
+        }
+
+        var request = new UpdateUserDataRequest
+        {
+            Data = new Dictionary<string, string>() { { No, data } },
+            Permission = UserDataPermission.Public
+        };
+        PlayFabClientAPI.UpdateUserData(request, (result) => {
+            print("데이터 저장 성공");
+            updateItemNo(No);
+            print(Player_Items[MyPlayFabInfo.PlayFabId]);
+        }, (error) => print("데이터 저장 실패"));
+    }
+
+    public void updateItemNo(string No)
+    {
+        int itemNo = int.Parse(No);
+        itemNo++;
+        string UpdatedNo = itemNo.ToString();
+
+        var request = new UpdateUserDataRequest
+        {
+            Data = new Dictionary<string, string>() { { "ItemNo", UpdatedNo } },
+            Permission = UserDataPermission.Public
+        };
+        PlayFabClientAPI.UpdateUserData(request, (result) => {
+            print("데이터 저장 성공");
+            Player_Items[MyPlayFabInfo.PlayFabId] = UpdatedNo;
+        }, (error) => print("데이터 저장 실패"));
+    }
+
+    public void getData()
+    {
+        GameObject g;
+        ItemTemplate = ShopScrollView.GetChild(0).gameObject;
+
+
+        for (int i = 0; i < PlayFabUserList.Count; i++)
+        {
+            string seller = PlayFabUserList[i].DisplayName;
+            print(seller);
+            var request = new GetUserDataRequest() { PlayFabId = PlayFabUserList[i].PlayFabId };
+            PlayFabClientAPI.GetUserData(request, (result) => {
+                foreach (var eachData in result.Data)
+                {
+                    if (!eachData.Value.Value.All(char.IsDigit))
+                    {
+                        string[] datas = eachData.Value.Value.Split('`');
+
+                        g = Instantiate(ItemTemplate, ShopScrollView);
+
+                        Text[] data_text = g.GetComponentsInChildren<Text>();
+                        print(seller);
+                        data_text[0].text = seller;
+                        data_text[1].text = datas[0];
+                        data_text[2].text = datas[1];
+                        data_text[3].text = datas[2];
+
+                        g.SetActive(true);
+                    }
+                }
+            }, (error) => print("실패"));
+
+        }
+    }
 
     void OnLoginSuccess(LoginResult result)
     {
         print("로그인 성공");
         Flag = true;
         LoginObject.SetActive(false);
-        register.SetActive(false);
-        purchase.SetActive(false);
 
         //온라인 연결 수행. 연결이 성공하면 콜백 메소드인 OnConnectedToMaster가 실행
         if (Flag == true)
         {
             PhotonNetwork.ConnectUsingSettings();
         }
+
+        GetLeaderboard(result.PlayFabId);
+
     }
+
+
+
+
+
+
+
 
     void OnLoginFailure(PlayFabError error) => print("로그인 실패");
 
-    void OnRegisterSuccess(RegisterPlayFabUserResult result) => print("회원가입 성공");
+
 
     void OnRegisterFailure(PlayFabError error) => print("회원가입 실패");
 
