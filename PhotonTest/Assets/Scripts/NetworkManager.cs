db12d25f42472efaf49ca079c093d0a333100b1e
KiBumYoon <62062154+YoonKiBum@users.noreply.github.com>
2022-05-31 01:10:05 +0900

    세상에 마상에

--- PhotonTest/Assets/Scripts/NetworkManager.cs
+++ PhotonTest/Assets/Scripts/NetworkManager.cs
@@ -1,4 +1,3 @@
-
 using UnityEngine;
 using PlayFab;
 using PlayFab.ClientModels;
@@ -10,15 +9,37 @@ using System.Collections;
 using System;
 using System.Linq;
 
-
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
-    public GameObject LoginObject;
-    public InputField EmailInput, PasswordInput, UsernameInput;
-    public InputField NameInput, PriceInput, InfoInput, SearchInput;
 
-    public Text StatusText;//접속상태
-                           // public InputField NickNameInput;//닉네임
+    [Header("LobbyPanel")]
+    public GameObject LobbyPanel;
+    public InputField RoomInput;
+    public Text WelcomeText;
+    public Text LobbyInfoText;
+    public Button[] CellBtn;
+    public Button PreviousBtn;
+    public Button NextBtn;
+
+    [Header("RoomPanel")]
+    public GameObject RoomPanel;
+    public Text ListText;
+    public Text RoomInfoText;
+    public Text[] ChatText;
+    public InputField ChatInput;
+
+    [Header("ETC")]
+    public Text StatusText;
+    public PhotonView PV;
+
+    List<RoomInfo> myList = new List<RoomInfo>();
+    int currentPage = 1, maxPage, multiple;
+
+    [Header("Remain")]
+    public GameObject LoginObject;
+    public InputField EmailInput, PasswordInput, UsernameInput, NicknameInput;
+    public InputField NameInput, PriceInput, InfoInput, SearchInput;
+  
     public GameObject Cube;//플레이어 생성 위치
     public string gameVersion = "1.0";//게임 버전
 
@@ -37,12 +58,53 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     public Dictionary<string, string> Player_Items = new Dictionary<string, string>(); // 각 플레이어가 등록한 Items의 Number
 
     bool Flag = false;
+    int cnt = 0;
 
-    void Awake()
+    #region 방리스트 갱신
+    // ◀버튼 -2 , ▶버튼 -1 , 셀 숫자
+    public void MyListClick(int num)
     {
-        //마스터 client와 일반 client의 레벨을 동기화 할지 결정한다. true일 시, 마스터 client에서 레벨 변경 시 모든 client들이 자동으로 동일한 레벨을 로드한다.
-        PhotonNetwork.AutomaticallySyncScene = true;
+        if (num == -2) --currentPage;
+        else if (num == -1) ++currentPage;
+        else PhotonNetwork.JoinRoom(myList[multiple + num].Name);
+        MyListRenewal();
     }
+
+    void MyListRenewal()
+    {
+        // 최대페이지
+        maxPage = (myList.Count % CellBtn.Length == 0) ? myList.Count / CellBtn.Length : myList.Count / CellBtn.Length + 1;
+
+        // 이전, 다음버튼
+        PreviousBtn.interactable = (currentPage <= 1) ? false : true;
+        NextBtn.interactable = (currentPage >= maxPage) ? false : true;
+
+        // 페이지에 맞는 리스트 대입
+        multiple = (currentPage - 1) * CellBtn.Length;
+        for (int i = 0; i < CellBtn.Length; i++)
+        {
+            CellBtn[i].interactable = (multiple + i < myList.Count) ? true : false;
+            CellBtn[i].transform.GetChild(0).GetComponent<Text>().text = (multiple + i < myList.Count) ? myList[multiple + i].Name : "";
+            CellBtn[i].transform.GetChild(1).GetComponent<Text>().text = (multiple + i < myList.Count) ? myList[multiple + i].PlayerCount + "/" + myList[multiple + i].MaxPlayers : "";
+        }
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        int roomCount = roomList.Count;
+        for (int i = 0; i < roomCount; i++)
+        {
+            if (!roomList[i].RemovedFromList)
+            {
+                if (!myList.Contains(roomList[i])) myList.Add(roomList[i]);
+                else myList[myList.IndexOf(roomList[i])] = roomList[i];
+            }
+            else if (myList.IndexOf(roomList[i]) != -1) myList.RemoveAt(myList.IndexOf(roomList[i]));
+        }
+        MyListRenewal();
+    }
+
+    #endregion
 
     void Start()
     {
@@ -262,22 +324,83 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 
     void OnRegisterFailure(PlayFabError error) => print("회원가입 실패");
 
-    //void Connect() => PhotonNetwork.ConnectUsingSettings();
+    public void OnClickChatting()
+    {
+        PhotonNetwork.LeaveRoom();
+        cnt++;
+    }
+
+    #region 서버연결
+    void Awake()
+    {
+        //마스터 client와 일반 client의 레벨을 동기화 할지 결정한다. true일 시, 마스터 client에서 레벨 변경 시 모든 client들이 자동으로 동일한 레벨을 로드한다.
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    void Update()
+    {
+        StatusText.text = PhotonNetwork.NetworkClientState.ToString();
+        LobbyInfoText.text = (PhotonNetwork.CountOfPlayers - PhotonNetwork.CountOfPlayersInRooms) + "로비 / " + PhotonNetwork.CountOfPlayers + "접속";
+    }
+    public void Connect() => PhotonNetwork.ConnectUsingSettings();
 
     public override void OnConnectedToMaster()
     {
         //생성된 방에 랜덤하게 접속한다. 
         //접속 실패 시, 콜백 메소드인 OnJoinRandomFailed가 실행되고 PhotonNetwork.CreateRoom을 통해 새로운 방을 생성한다. 
         //접속 성공 시, OnJoinedRoom가 실행되고, PhotonNetwork.Instantiate을 통해 플레이어를 생성한다.
-        PhotonNetwork.JoinRandomRoom();
+        PhotonNetwork.JoinLobby();
     }
+
+    public override void OnJoinedLobby()
+    {
+        LobbyPanel.SetActive(true);
+        RoomPanel.SetActive(false);
+        PhotonNetwork.LocalPlayer.NickName = NicknameInput.text;
+        WelcomeText.text = PhotonNetwork.LocalPlayer.NickName + "님 환영합니다";
+        myList.Clear();
+    }
+
+    public void Disconnect() => PhotonNetwork.Disconnect();
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        LobbyPanel.SetActive(false);
+        RoomPanel.SetActive(false);
+    }
+    #endregion
+
+    #region 방
+    public void CreateRoom()
+    {
+        //PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 4 });
+        PhotonNetwork.CreateRoom(RoomInput.text == "" ? "Room" + UnityEngine.Random.Range(0, 100) : RoomInput.text, new RoomOptions { MaxPlayers = 4 });
+    }
+
+    public void MapBtn()
+    {
+        PhotonNetwork.JoinRandomRoom();
+        LobbyPanel.SetActive(false);
+        RoomPanel.SetActive(false);
+        cnt++;
+    }
+
+    public void LeaveRoom() => PhotonNetwork.LeaveRoom();
 
     public override void OnJoinedRoom()
     {
-        //PhotonNetwork.Instantiate("Jammo_Player", Cube.transform.position, Quaternion.identity);
         PhotonNetwork.Instantiate("Player3", Cube.transform.position, Quaternion.identity);
         Debug.Log("Joined");
+        if (cnt % 2 == 0)
+        {
+            RoomPanel.SetActive(true);
+            RoomRenewal();
+            ChatInput.text = "";
+            for (int i = 0; i < ChatText.Length; i++) ChatText[i].text = "";
+        }
     }
+
+    public override void OnCreateRoomFailed(short returnCode, string message) { RoomInput.text = ""; CreateRoom(); }
 
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
@@ -285,8 +408,51 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         Debug.Log("Failed! Create new room");
     }
 
-    void CreateRoom()
+    public override void OnPlayerEnteredRoom(Player newPlayer)
     {
-        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 4 });
+        RoomRenewal();
+        ChatRPC("<color=yellow>" + newPlayer.NickName + "님이 참가하셨습니다</color>");
     }
+
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        RoomRenewal();
+        ChatRPC("<color=yellow>" + otherPlayer.NickName + "님이 퇴장하셨습니다</color>");
+    }
+
+    void RoomRenewal()
+    {
+        ListText.text = "";
+        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+            ListText.text += PhotonNetwork.PlayerList[i].NickName + ((i + 1 == PhotonNetwork.PlayerList.Length) ? "" : ", ");
+        RoomInfoText.text = PhotonNetwork.CurrentRoom.Name + " / " + PhotonNetwork.CurrentRoom.PlayerCount + "명 / " + PhotonNetwork.CurrentRoom.MaxPlayers + "최대";
+    }
+    #endregion
+
+    #region 채팅
+    public void Send()
+    {
+        PV.RPC("ChatRPC", RpcTarget.All, PhotonNetwork.NickName + " : " + ChatInput.text);
+        ChatInput.text = "";
+    }
+
+    [PunRPC] // RPC는 플레이어가 속해있는 방 모든 인원에게 전달한다
+    void ChatRPC(string msg)
+    {
+        bool isInput = false;
+        for (int i = 0; i < ChatText.Length; i++)
+            if (ChatText[i].text == "")
+            {
+                isInput = true;
+                ChatText[i].text = msg;
+                break;
+            }
+        if (!isInput) // 꽉차면 한칸씩 위로 올림
+        {
+            for (int i = 1; i < ChatText.Length; i++) ChatText[i - 1].text = ChatText[i].text;
+            ChatText[ChatText.Length - 1].text = msg;
+        }
+    }
+    #endregion
 }
