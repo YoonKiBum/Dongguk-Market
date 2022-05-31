7f418b6355e570ef3bc5f4738e4d3c91f2df7c2f
EHEH1357 <s2dorns2@dongguk.edu>
2022-05-31 16:43:18 +0900

    Error Edited
    마스터서버 접속 오류 해결

--- PhotonTest/Assets/Scripts/CamController.cs
+++ PhotonTest/Assets/Scripts/CamController.cs
@@ -18,33 +18,35 @@ public class CamController : MonoBehaviour {
     
     // Update is called once per frame 
     void Update() { 
-    	// 마우스 우클릭 중에만 카메라 무빙 적용 
-        if (Input.GetMouseButton(1)) { 
-        	xmove += Input.GetAxis("Mouse X"); 
-            // 마우스의 좌우 이동량을 xmove 에 누적합니다. 
-            ymove -= Input.GetAxis("Mouse Y"); 
-            // 마우스의 상하 이동량을 ymove 에 누적합니다. 
-        } 
-        transform.rotation = Quaternion.Euler(ymove, xmove, 0); // 이동량에 따라 카메라의 바라보는 방향을 조정합니다. 
-        if (Input.GetMouseButtonDown(2)) toggleView = 4 - toggleView; 
-        if (toggleView == 3) { 
-        	distance -= Input.GetAxis("Mouse ScrollWheel") * wheelspeed; 
-            if (distance < 1f) distance = 1f; 
-            if (distance > 100.0f) distance = 100.0f; 
-        } 
-        if (toggleView == 1) { 
-        	Vector3 Eye = player.transform.position + Player_Height; 
-            Vector3 reverseDistance = new Vector3(-5.0f, -3.0f, 10.0f); 
-            // 카메라가 바라보는 앞방향은 Z 축입니다. 이동량에 따른 Z 축방향의 벡터를 구합니다. 
-            transform.position = Eye + transform.rotation * reverseDistance; 
-            // 플레이어의 위치에서 카메라가 바라보는 방향에 벡터값을 적용한 상대 좌표를 차감합니다. 
-        } 
-        else if (toggleView == 3) { 
-        	Vector3 Eye = player.transform.position 
-            	+ transform.rotation * Player_Side + Player_Height; 
-            Vector3 reverseDistance = new Vector3(0.0f, 0.0f, distance); 
-            // 카메라가 바라보는 앞방향은 Z 축입니다. 이동량에 따른 Z 축방향의 벡터를 구합니다. 
-            transform.position = Eye - transform.rotation * reverseDistance; 
-        } 
+        if(player != null){
+            // 마우스 우클릭 중에만 카메라 무빙 적용 
+            if (Input.GetMouseButton(1)) { 
+                xmove += Input.GetAxis("Mouse X"); 
+                // 마우스의 좌우 이동량을 xmove 에 누적합니다. 
+                ymove -= Input.GetAxis("Mouse Y"); 
+                // 마우스의 상하 이동량을 ymove 에 누적합니다. 
+            } 
+            transform.rotation = Quaternion.Euler(ymove, xmove, 0); // 이동량에 따라 카메라의 바라보는 방향을 조정합니다. 
+            if (Input.GetMouseButtonDown(2)) toggleView = 4 - toggleView; 
+            if (toggleView == 3) { 
+                distance -= Input.GetAxis("Mouse ScrollWheel") * wheelspeed; 
+                if (distance < 1f) distance = 1f; 
+                if (distance > 100.0f) distance = 100.0f; 
+            } 
+            if (toggleView == 1) { 
+                Vector3 Eye = player.transform.position + Player_Height; 
+                Vector3 reverseDistance = new Vector3(-5.0f, -3.0f, 10.0f); 
+                // 카메라가 바라보는 앞방향은 Z 축입니다. 이동량에 따른 Z 축방향의 벡터를 구합니다. 
+                transform.position = Eye + transform.rotation * reverseDistance; 
+                // 플레이어의 위치에서 카메라가 바라보는 방향에 벡터값을 적용한 상대 좌표를 차감합니다. 
+            } 
+            else if (toggleView == 3) { 
+                Vector3 Eye = player.transform.position 
+                    + transform.rotation * Player_Side + Player_Height; 
+                Vector3 reverseDistance = new Vector3(0.0f, 0.0f, distance); 
+                // 카메라가 바라보는 앞방향은 Z 축입니다. 이동량에 따른 Z 축방향의 벡터를 구합니다. 
+                transform.position = Eye - transform.rotation * reverseDistance; 
+            } 
+        }
     } 
  }
